224ad58234419949a4eab689193a11381eeb26c6
kenney84 <tkenney930w@gmail.com>
2024-01-25 11:17:18 -0500

    Added Capacitor for Mobile Build & Deployments

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cmsc-480',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
