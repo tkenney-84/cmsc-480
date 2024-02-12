1f0332d1fa0bea1a2b0958b8f7448f19c51f402b
kenney84 <tkenney930w@gmail.com>
2024-02-12 03:28:12 -0500

    Finish DevOps
    - Add database integration.
    - Add CORS integration.
    - Watch database for errors and process closure.
    - Finish environment configurations.
    - Setup test environment.

--- src-backend/routes/index.js
+++ src-backend/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var connection = global.database;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,8 +8,26 @@ router.get('/', function(req, res, next) {
 });
 
 // Returns a string to verify that GET requests to the server are working.
-router.get('/test', function(req, res, next) {
-  res.send('GET requests to the server are working.');
+router.get('/testGet', function(req, res, next) {
+  var query = 'SELECT * FROM test_table WHERE id = ?;';
+  if (req.query.record == "") {
+    res.send(JSON.stringify("No record specified."));
+  } else {
+    connection.query(query, [req.query.record], function(err, results) {
+      res.send(JSON.stringify(results[0].name));
+    });
+  }
+});
+
+router.post('/testPost', function(req, res) {
+  var query = 'INSERT INTO test_table (name) VALUES (?);';
+  if (!req.body.name) {
+    res.send(JSON.stringify("No name specified."));
+  } else {
+    connection.query(query, [req.body.name], function(err, rows, fields) {
+      res.send(JSON.stringify(rows.insertId));
+    });
+  }
 });
 
 module.exports = router;
