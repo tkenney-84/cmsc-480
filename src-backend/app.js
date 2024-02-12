a68dd198c5913a46e4129cb76334004047e1cc6c
kenney84 <tkenney930w@gmail.com>
2024-02-12 03:28:12 -0500

    Finish DevOps
    - Add database integration.
    - Add CORS integration.
    - Watch database for errors and process closure.
    - Finish environment configurations.
    - Setup test environment.

--- src-backend/app.js
+++ src-backend/app.js
@@ -3,11 +3,34 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql = require('mysql2');
+
+var app = express();
+
+global.database = mysql.createConnection({
+  host     : process.env.DATABASE_HOST,
+  port     : process.env.DATABASE_PORT,
+  user     : process.env.DATABASE_USER,
+  password : process.env.DATABASE_PASSWORD,
+  database : process.env.DATABASE_NAME,
+  sslmode  : 'REQUIRED',
+});
+
+console.log("DATABASE", global.database);
+
+global.database.connect(function(err) {
+  console.error('Error connecting to MySQL:', err);
+});
+
+var cors = require('cors');
+var corsOptions = {
+  origin: process.env.ALLOWED_ORIGIN,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions));
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -36,6 +59,29 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// app.listen(process.env.PORT);
+
+process.on('SIGINT', function() {
+  global.database.end(function(err) {
+    if (err) {
+      console.error('Failed to close MySQL connection:', err);
+    } else {
+      console.log('MySQL connection closed.');
+    }
+    process.exit();
+  });
+});
+
+global.database.on('error', function(err) {
+  console.error('Database error:', err);
+
+  if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+    global.database = mysql.createConnection(global.database.config);
+  } else {
+    throw err;
+  }
 });
 
 module.exports = app;
