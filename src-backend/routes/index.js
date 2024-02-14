37f7a698524a98f788ab0b84ee1fc49c4b5b7189
wgllagher44 <willgallagher0702@gmail.com>
2024-02-14 14:05:13 -0500

    Sprint 0
    Send Get Request to server to get an array of random numbers. Made a Post request to the get the solar position based on latitude and Longitude

--- src-backend/routes/index.js
+++ src-backend/routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var connection = global.database;
-
+var axios = require('axios')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'CMSC480' });
 });
 
-// Returns a string to verify that GET requests to the server are working.
+//Returns a string to verify that GET requests to the server are working.
 router.get('/testGet', function(req, res, next) {
   var query = 'SELECT * FROM test_table WHERE id = ?;';
   if (req.query.record == "") {
@@ -29,5 +29,32 @@ router.post('/testPost', function(req, res) {
     });
   }
 });
+//sends an array of random numbers to test get request to the front end.
+router.get('/sendRandomNumbers',function(req,res){
+      var result = [];
+      for(let i = 0; i < 25; i++){
+        result.push(Math.floor(Math.random() * (100 - 1) + 1));
+      }
+      result.sort(function(a,b){return a- b});
+      console.log(result);
+      res.send({randomNumbers:result});
+
+})
+router.post('/solarCoordinates',async function(req,res){
+  console.log(req.body)
+  if(req.body.lat == '' || req.body.long == ''){
+    res.send(JSON.stringify("No altitude or longitude specified"))
+  }else{
+    //eventually Store this in the data base 
+    var key ='d9e2f832f360421b93fa7250f9a2ef72';
+    var apiEndpoint =`https://api.ipgeolocation.io/astronomy?apiKey=${key}&lat=${req.body.lat}&long=${req.body.long}`; 
+    var altitude = "";
+    var azimuth = "";
+    await axios.get(apiEndpoint).then((response)=>{
+        console.log(response)
+        res.send({sunAltitude:response.data.sun_altitude,sunAzimuth:response.data.sun_azimuth});
+    })  
+  }
+})
 
 module.exports = router;
--- src/app/mainpage/mainpage.page.ts
+++ src/app/mainpage/mainpage.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
+import { environment } from 'src/environments/environment.local';
 
 @Component({
   selector: 'app-mainpage',
@@ -8,7 +10,31 @@ import { Component, OnInit } from '@angular/core';
 export class MainpagePage implements OnInit {
 
   constructor() { }
+  baseURL: string = environment.apiURL;
+  randomNumbers:string[] = [];
+  lat:number = 0;
+  long:number = 0;
+  altitude:number = 0;
+  azimuth:number =0;
+  getRandomNumbers(){
+    axios.get(`${this.baseURL}/sendRandomNumbers`).then((response)=>{
+      let arr = response.data.randomNumbers;
+     
+      this.randomNumbers = arr;
+    })
+  }
+  sendLatLong(){
 
+
+    axios.post(`${this.baseURL}/solarCoordinates`,{
+      lat:this.lat,
+      long:this.long
+    }).then((response) =>{
+      this.altitude = response.data.sunAltitude;
+      this.azimuth = response.data.sunAzimuth;
+      console.log(response.data);
+    })
+  }
   ngOnInit() {
   }
 
