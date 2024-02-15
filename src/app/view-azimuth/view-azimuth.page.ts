d30cc24da94ab679bec8f331518f1cb68e569950
lmartin26404 <lukemartin3507@gmail.com>
2024-02-15 17:33:12 -0500

    [Sprint #0] Drop Down Menu
    Creates a drop down menu for the azimuth page, it is for FAQ.

--- src/app/view-azimuth/view-azimuth.page.ts
+++ src/app/view-azimuth/view-azimuth.page.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewAzimuthPage implements OnInit {
 
+  isDivVisible:boolean = false;
+  toggleDiv()
+  {
+    this.isDivVisible = !this.isDivVisible;
+  }
+
+  hide1:boolean = true;
+  ngIfCtrl1(){
+    this.hide1 = !this.hide1;
+  }
+
+  hide2:boolean = true;
+  ngIfCtrl2(){
+    this.hide2 = !this.hide2;
+  }
+
+
   constructor() { }
 
   ngOnInit() {
