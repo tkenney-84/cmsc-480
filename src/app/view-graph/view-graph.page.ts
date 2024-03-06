87373cd1030972b24887ea5e543088ae58fbd682
lmartin26404 <lukemartin3507@gmail.com>
2024-03-06 16:33:18 -0500

    [Sprit 2] Graphs

--- src/app/view-graph/view-graph.page.ts
+++ src/app/view-graph/view-graph.page.ts
@@ -1,54 +1,39 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { Chart } from 'chart.js';
+import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js/auto'; // Importing Chart from 'chart.js/auto'
 
 @Component({
   selector: 'app-view-graph',
   templateUrl: 'view-graph.page.html',
   styleUrls: ['view-graph.page.scss'],
 })
-export class ViewGraphPage implements AfterViewInit {
-  // Importing ViewChild. We need @ViewChild decorator to get a reference to the local variable 
-  // that we have added to the canvas element in the HTML template.
-  @ViewChild('barCanvas') private barCanvas!: ElementRef;
-
-  barChart: any;
-
+export class ViewGraphPage implements OnInit {
+  
   constructor() { }
 
-  // When we try to call our chart to initialize methods in ngOnInit() it shows an error nativeElement of undefined. 
-  // So, we need to call all chart methods in ngAfterViewInit() where @ViewChild and @ViewChildren will be resolved.
-  ngAfterViewInit() {
-    this.barChartMethod();
+  ngOnInit() {
+    this.showChart();
   }
 
-  barChartMethod() {
-    // Now we need to supply a Chart element reference with an object that defines the type of chart we want to use, and the type of data we want to display.
-    this.barChart = new Chart(this.barCanvas.nativeElement, {
+  showChart() {
+    var ctx = document.getElementById('chartPower') as HTMLCanvasElement;
+    var chart = new Chart(ctx, {
       type: 'bar',
       data: {
-        labels: ['BJP', 'INC', 'AAP', 'CPI', 'CPI-M', 'NCP'],
+        labels:['AV','VA'],
         datasets: [{
-          label: '# of Votes',
-          data: [200, 50, 30, 15, 20, 34],
+          label: "Power",
           backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
+            'rgba(255,99,132,0.2)', // Corrected color values
+            'rgba(233,93,123,0.2)'
           ],
           borderColor: [
             'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
+            'rgba(255,99,132,1)'
           ],
+          data:[20,80],
           borderWidth: 1
         }]
-      },
+      }
     });
   }
 }
