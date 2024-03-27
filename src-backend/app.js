b3148e78f788a6fefc2bd3fcad34870479f2f510
Tyler Kenney <kenney@tyler-desktop-debian.kenneydiaz.net>
2024-03-27 04:03:57 -0400

    [Sprint 3] Added Rate-Limiting and Improved Maintainability of Backend Code

--- src-backend/app.js
+++ src-backend/app.js
@@ -1,97 +1,159 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mysql = require('mysql2');
+// Import all necessary packages.
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mysql = require("mysql2");
+const rateLimiter = require("express-rate-limit");
+var cors = require("cors");
 
+// Declare a new instance of the express NodeJS framework for use as our
+// backend server.
 var app = express();
 
+// Create database connection for this instance of the backend server.
+// This connection is stored globally for use across all route js files.
 global.database = mysql.createConnection({
-  host     : process.env.DATABASE_HOST,
-  port     : process.env.DATABASE_PORT,
-  user     : process.env.DATABASE_USER,
-  password : process.env.DATABASE_PASSWORD,
-  database : process.env.DATABASE_NAME,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   multipleStatements: false,
-  // sslmode  : 'REQUIRED' // DO NOT RE ENABLE THIS!
+  // Do not add an 'sslmode' property. It is not needed or supported by mysql2.
 });
 
-global.schema = global.database.config.database || "no-schema-found";
+// Log the status of the database connection to whatever terminal launched the
+// backend server.
+console.info("DATABASE CONFIGURED?", global.database.config.user != null);
 
-console.log("DATABASE CONFIGURED?", global.database.config.user != null);
-
-global.database.connect(function(err) {
+// Connect to the database using the connection object created above.
+// If an error occurs, log the error and kill the server.
+global.database.connect(function (err) {
   if (err) {
-    console.error('Error connecting to MySQL:', err);
-    process.exit();
+    console.error("Error connecting to MySQL:", err);
+    // process.exit(); Removed for production server persistence.
+    // It is better to live with a DB error than to kill the entire backend
+    // server.
   }
 });
 
-var cors = require('cors');
+// Enable CORS for all routes. This is necessary for the frontend to be able to
+// make requests to the backend server. If this is modified, CORS errors will
+// block all requests from the frontend to this backend server.
 var corsOptions = {
   origin: process.env.ALLOWED_ORIGIN,
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+  optionsSuccessStatus: 200, // Necessary for legacy browser support.
+};
+
+// Apply CORS options to all application requests.
 app.use(cors(corsOptions));
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var solarPanelMovement = require('./routes/solarPanelControl')
-var solarData = require('./routes/solarData')
+// Implement basic rate limiting for all requests to the backend server.
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// Limit each origin IP to 'maximumRequests' requests per 'perMilliseconds'
+// milliseconds.
+var maximumRequests = 100;
+var perMilliseconds = 60 * 1000;
 
-app.use(logger('dev'));
+// Create a rate limiter object with the above parameters.
+const apiLimiter = rateLimiter({
+  windowMs: perMilliseconds,
+  max: maximumRequests,
+  handler: function (req, res, next) {
+    res.status(429);
+    res.send({
+      success: false,
+      message: `The server has received too many requests in a short period, please try again later.`,
+    });
+  },
+});
+
+// Apply the rate limiter to all requests to this backend server..
+app.use(apiLimiter);
+
+// Import all route files for the backend server.
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var solarPanelMovement = require("./routes/solarPanelControl");
+var solarData = require("./routes/solarData");
+
+// Support for view functionality offered by ExpressJS.
+// Not explicitly used in this project, but left in for fear of dependency.
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+// Middleware for parsing incoming requests.
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/solarPanelControl',solarPanelMovement)
-app.use('/solarData',solarData)
+// Declare base URL stub for all route files.
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/solarPanelControl", solarPanelMovement);
+app.use("/solarData", solarData);
 
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Handle requests to unknown routes.
+app.use(function (req, res, next) {
+  // next(createError(404));
+  res.status(404);
+  res.send({
+    success: false,
+    message: "API endpoint not found.",
+  });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+// Handle errors with the backend server.
+app.use(function (err, req, res, next) {
+  // Set locals, only providing error in development.
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error =
+    process.env.ENVIRONMENT === "local"
+      ? err
+      : "Request error present, but obscured for security reasons. Please check the server logs for more information.";
 
-  // render the error page
+  // If there is a status associated with this error, send it in the response.
+  // Otherwise send a general 500 server error.
   res.status(err.status || 500);
-  res.render('error');
+
+  // Do not render the error page. All views should be handled by the front end.
+  // res.render('error');
+
+  res.send({
+    success: false,
+    message: "An error occurred while processing the request.",
+    error: res.locals.error,
+  });
 });
 
-// app.listen(process.env.PORT);
-
-process.on('SIGINT', function() {
-  global.database.end(function(err) {
+// Whenever this backend server is killed, close the MySQL connection.
+process.on("SIGINT", function () {
+  global.database.end(function (err) {
     if (err) {
-      console.error('Failed to close MySQL connection:', err);
+      console.error("Failed to close MySQL connection:", err);
     } else {
-      console.log('MySQL connection closed.');
+      console.info("MySQL connection closed.");
     }
+
+    // It is fine to kill the process here, as if we have reached this block,
+    // it is what is intended.
     process.exit();
   });
 });
 
-global.database.on('error', function(err) {
-  console.error('Database error:', err);
-
-  if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+global.database.on("error", function (err) {
+  // If the error is a connection error, attempt to reconnect.
+  if (err.code === "PROTOCOL_CONNECTION_LOST") {
+    console.warn("Database connection lost. Attempting to reconnect...");
     global.database = mysql.createConnection(global.database.config);
   } else {
-    throw err;
+    // throw err; Removed for production server persistence.
+    // It is better to live with a DB error than to kill the entire backend.
+    console.error("Database Error:", err);
   }
 });
 
